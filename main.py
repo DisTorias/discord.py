593e8d23ab60e2b985ededbe80da190cd3f11bf3
DisTorias <121464235+DisTorias@users.noreply.github.com>
2022-12-26 15:27:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import discord.py
 from discord.ext import commands
 
 bot = commadns.Bot(command_prefix="s!', intents = intents = discord.Intents().all())
+
+@bot.event
+async def on_ready():
+    print("Бот Активировался!")
